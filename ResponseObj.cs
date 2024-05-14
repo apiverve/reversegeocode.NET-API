6de33acf69f178dbdbf3e15f5c6006c8523cbabf
APIVerve Team <hello@apiverve.com>
2024-05-14 15:40:04 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,65 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class data
+{
+    [JsonProperty("zipcode")]
+    public string zipcode { get; set; }
+
+    [JsonProperty("state_abbr")]
+    public string stateabbr { get; set; }
+
+    [JsonProperty("city")]
+    public string city { get; set; }
+
+    [JsonProperty("state")]
+    public string state { get; set; }
+
+    [JsonProperty("distance")]
+    public double distance { get; set; }
+
+    [JsonProperty("latitudeClosest")]
+    public string latitudeClosest { get; set; }
+
+    [JsonProperty("longitudeClosest")]
+    public string longitudeClosest { get; set; }
+
+    [JsonProperty("countryCode")]
+    public string countryCode { get; set; }
+
+    [JsonProperty("latitudeClosestCity")]
+    public object latitudeClosestCity { get; set; }
+
+    [JsonProperty("longitudeClosestCity")]
+    public object longitudeClosestCity { get; set; }
+
+    [JsonProperty("latitude")]
+    public double latitude { get; set; }
+
+    [JsonProperty("longitude")]
+    public double longitude { get; set; }
+
+    [JsonProperty("estimatedCity")]
+    public bool estimatedCity { get; set; }
+
+    [JsonProperty("nearestCities")]
+    public string[] nearestCities { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
